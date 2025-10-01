f44d47761271ef7e83bebde3f6f8e1bae1a9335a
David Chávez Rodríguez <davidtaim@hotmail.com>
2025-10-01 17:35:14 -0600

    fix: added missing repository annotation

--- src/main/java/mx/dvdchr/invitation_management/repository/EventSeatRepository.java
+++ src/main/java/mx/dvdchr/invitation_management/repository/EventSeatRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import mx.dvdchr.invitation_management.model.Event;
 import mx.dvdchr.invitation_management.model.EventSeat;
 
+@Repository
 public interface EventSeatRepository extends JpaRepository<EventSeat, UUID> {
     List<EventSeat> findByEvent(Event event);
     List<EventSeat> findByEventAndIsAvailable(Event event, Boolean isAvailable);
--- src/main/java/mx/dvdchr/invitation_management/repository/InvitationGuestRepository.java
+++ src/main/java/mx/dvdchr/invitation_management/repository/InvitationGuestRepository.java
@@ -3,12 +3,14 @@ package mx.dvdchr.invitation_management.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import mx.dvdchr.invitation_management.model.Guest;
 import mx.dvdchr.invitation_management.model.Invitation;
 import mx.dvdchr.invitation_management.model.InvitationGuest;
 import mx.dvdchr.invitation_management.model.compositePrimaryKey.InvitationGuestId;
 
+@Repository
 public interface InvitationGuestRepository extends JpaRepository<InvitationGuest, InvitationGuestId> {
     List<InvitationGuest> findByInvitation(Invitation invitation);
     InvitationGuest findByGuest(Guest guest);
